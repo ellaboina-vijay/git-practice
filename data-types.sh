4c2a57ac5c719802c1e093785fec3d9f33e645ad
vijay <ellaboina>
2024-10-28 15:11:35 +0530

    added Date Types

--- data-types.sh
+++ data-types.sh
@@ -1,16 +1,17 @@
  #!/bin/bash
 
-# NUMBER1=$1
-# NUMBER2=$2
-
-# SUM=$(($NUBER1+$NUMBER2))
-# echo "Total of $NUMBER1 and $NUMBER2 is : $SUM"
-
-
-
 NUMBER1=$1
 NUMBER2=$2
 
-SUM=$(($NUMBER1+$NUMBER2))
+SUM=$(($NUBER1+$NUMBER2))
 
-echo "Total of $NUMBER1 and $NUMBER2 is: $SUM"
+echo "Total of $NUMBER1 and $NUMBER2 is : $SUM"
+
+
+
+# NUMBER1=$1
+# NUMBER2=$2
+
+# SUM=$(($NUMBER1+$NUMBER2))
+
+# echo "Total of $NUMBER1 and $NUMBER2 is: $SUM"
